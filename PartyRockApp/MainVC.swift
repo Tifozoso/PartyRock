1aa0f8db0c850ad1d60dfa25940d2463b994634b
Tifozoso <mfranks@gmail.com>
2017-09-26 14:12:24 -0700

    Completed 61: - Basic layout - Table View - Video VC & Link

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Michael Franks on 9/26/17.
+//  Copyright © 2017 Popcorn Labs. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
